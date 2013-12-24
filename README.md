33cb837f578d2029286399ca0c1bdeaf5c89c03e
Daniel Trostli <daniel.trostli@gmail.com>
2013-12-24 17:45:15 -0200

    Initial commit with updated gemfile and PG databse

---
+++ README.md
@@ -0,0 +1,8 @@
+# ProMapper
+
+## Padmapper for Jobs
+
+Built with Rails 4 and the Google Maps API
+
+Trello Board:
+https://trello.com/b/Ej0FMNen/promapper
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProMapper::Application.config.secret_key_base = '52f2b09dfa88bb79c8436001e7a3c6071872669f6aa8a5283d9721fb58f71af8f6e6640aa05018eff356185c975b34b60c3b972f043f1e30ffcd84f491d6c627'
