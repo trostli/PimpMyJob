387f7343a7756ea99aeb2eacdf86938d736059e4
Daniel Trostli <daniel.trostli@gmail.com>
2013-12-26 00:28:19 -0200

    Added category array to seed file and auto create one category

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,21 @@ def location_helper(coordinate_type)
   end
 end
 
-def category_helper
-  %w(accounting+finance admin/office arch/engineering art/media/design biotech/science business/mgmt customer service education food/bev/hosp general labor government human resources internet engineers legal/paralegal manufacturing marketing/pr/ad medical/health nonprofit sector real estate retail/wholesale sales/biz dev salon/spa/fitness security skilled trade/craft software/qa/dba systems/network technical support transport tv/film/video web/info design writing/editing).sample
+
+@categories = %w(accounting+finance admin/office arch/engineering art/media/design biotech/science business/mgmt customer service education food/bev/hosp general labor government human resources internet engineers legal/paralegal manufacturing marketing/pr/ad medical/health nonprofit sector real estate retail/wholesale sales/biz dev salon/spa/fitness security skilled trade/craft software/qa/dba systems/network technical support transport tv/film/video web/info design writing/editing)
+
+
+@categories.each do |category|
+  Category.create(name: category)
 end
 
-
 200.times do |i|
+  category_name = category_helper
+
   job_attributes = {
+    :category_id: rand(@categories.length)+1
     :title => i,
+    :company => Faker::Company.name,
     :compensation => "$" + Faker::Number(2).to_s + " an hour",
     :description => Faker::Lorem.paragraph,
     :address => Faker::Address.street_address,
