a3353371e8eb3dfa3be911b129c22fb5268255f0
Daniel Trostli <daniel.trostli@gmail.com>
2013-12-26 00:12:50 -0200

    Added basic seed file, still neds to be updated to correctly create job with associated category

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,32 +4,32 @@ def location_helper(coordinate_type)
   case coordinate_type
   when :latitude
     if chance == 2
-      (37.768815 * (rand/1000 + 1))
+      (32.742815 * (rand/1000 + 1))
     elsif chance == 1
-      (37.768815 * (-rand/1000 + 1))
+      (32.742815 * (-rand/1000 + 1))
     end
   when :longitude
       if chance == 2
-      (-122.439736 * (rand/2000 + 1))
+      (-117.141091 * (rand/1000 + 1))
     elsif chance == 1
-      (-122.439736 * (-rand/2000 + 1))
+      (-117.141091 * (-rand/1000 + 1))
     end
   end
 end
 
-
-200.times do |i|
-  photo_attributes = {
-    :insta_id => i,
-    :latitude => location_helper(:latitude),
-    :longitude => location_helper(:longitude),
-    :location_name => Faker::Address.street_name,
-    :created_time => rand(86400) + 1383202800,
-    :like_count => rand(20),
-    :link => 'http://instagram.com/p/gKaFkat0Tj/' ,
-    :thumbnail_url => "http://origincache-prn.fbcdn.net/1173089_566382760098534_103778226_s.jpg",
-    :caption => Faker::Lorem.words.map { |word| "#"+word }.join(" ")
-  }
-  Photo.create(photo_attributes)
+def category_helper
+  %w(accounting+finance admin/office arch/engineering art/media/design biotech/science business/mgmt customer service education food/bev/hosp general labor government human resources internet engineers legal/paralegal manufacturing marketing/pr/ad medical/health nonprofit sector real estate retail/wholesale sales/biz dev salon/spa/fitness security skilled trade/craft software/qa/dba systems/network technical support transport tv/film/video web/info design writing/editing).sample
 end
 
+
+200.times do |i|
+  job_attributes = {
+    :title => i,
+    :compensation => "$" + Faker::Number(2).to_s + " an hour",
+    :description => Faker::Lorem.paragraph,
+    :address => Faker::Address.street_address,
+    :latitude => location_helper(:latitude),
+    :longitude => location_helper(:longitude),
+  }
+  Job.create(job_attributes)
+end
