eec21e84c86edd0278fc2ba74d90e8e10d61db1b
Nishant Chandra <nishantchandra1@gmail.com>
2013-12-28 02:07:51 -0800

    change map from SF to SD

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -5,11 +5,11 @@ proMapper.infoWindow = new google.maps.InfoWindow;
 
 proMapper.makeMap = function() {
   var mapOptions = {
-    center: new google.maps.LatLng(37.7833, -122.4167),
+    center: new google.maps.LatLng(32.7150, -117.1625),
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  proMapper.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  proMapper.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
 
 
